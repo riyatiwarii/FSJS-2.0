339df72373a5054c6040db9108aeadc1f034cbcf
Riya Tiwari <riyatiwari727@gmail.com>
2022-12-20 02:15:54 +0530

    practice

--- HTML & CSS Assignments/02_Project- Hosting Site Landing Page/index.js
+++ HTML & CSS Assignments/02_Project- Hosting Site Landing Page/index.js
@@ -1,3 +1,10 @@
-const btn = document.getElementById('ham-btn');
-const menu = document.getElementById('nav-options');
-console.log(btn);
+const hamburger = document.getElementById('ham-btn');
+const navoptions = document.getElementById('nav-options');
+console.log(hamburger);
+
+function navToggle(){
+    hamburger.classList.toggle('open');
+    navoptions.classList.toggle('show-options')
+}
+
+btn.addEventListener('click', navToggle);
