6d05004850906b164c43dcf974274fd0de2498d9
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-03 03:21:00 +0530

    menubar-hovereffects-done

--- HTML & CSS Assignments/05_Project- Beats Landing Page/index.js
+++ HTML & CSS Assignments/05_Project- Beats Landing Page/index.js
@@ -3,3 +3,11 @@ scrollButton.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
 
+const togglebar = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navlist");
+
+togglebar.addEventListener("click", () => {
+  togglebar.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
