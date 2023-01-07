4d61de08a8fa198b8b006c0b80f5ff0ccc624fee
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-07 19:21:06 +0530

    practice

---
+++ Javascript/index.js
@@ -0,0 +1,18 @@
+const nums = [2, 3, 4, 5, 6];
+// Numberlist element*10
+const multiply = nums.map(element => element*10);
+console.log(multiply);
+
+var x = 10;
+
+function riyaSays(){
+	console.log("Riya says Hello.");
+  	riyaExits();
+    console.log("Riya exited.")
+}
+
+function riyaExits(){
+  console.log("Riya exits."); 
+} 
+
+riyaSays();
