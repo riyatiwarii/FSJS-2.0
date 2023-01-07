6e15286fc60f756362e5161226c17ec31a8212a1
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-07 19:28:44 +0530

    code-for-article

---
+++ Javascript/index1.js
@@ -0,0 +1,9 @@
+var num1 = 4;
+var num2 = 7;
+function multiply(x, y){
+    var res = x*y;
+    return res
+};
+var resultOne = multiply(num1, num2);
+var resultTwo = multiply(7, 7);
+console.log(resultOne, resultTwo);
