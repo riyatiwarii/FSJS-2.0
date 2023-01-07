dfbdd0aa2116991712adf5cbee5395c17cd3815e
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-08 02:29:30 +0530

    added

--- Javascript/index1.js
+++ Javascript/index1.js
@@ -1,9 +1,11 @@
-// var num1 = 4;
-// var num2 = 7;
-// function multiply(x, y){
-//     var res = x*y;
-//     return res
-// };
-// var resultOne = multiply(num1, num2);
-// var resultTwo = multiply(7, 7);
-// console.log(resultOne, resultTwo);
+var num1 = 4;
+var num2 = 7;
+function multiply(x, y){
+    var res = x*y;
+    return res
+};
+var resultOne = multiply(num1, num2);
+var resultTwo = multiply(7, 7);
+console.log(resultOne, resultTwo);
+
+
