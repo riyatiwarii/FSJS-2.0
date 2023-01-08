a56fa1540be4ba6cc25b39b03cd47c75076c0711
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-09 04:15:03 +0530

    code-first

---
+++ Javascript/object2.js
@@ -0,0 +1,11 @@
+// Creating object for storing student details
+
+const studentDetail = {
+    studentName: "Alex",
+    studentRollNo: 7,
+    studentCourse: "JavaScript",
+    presentAttendance: true;    
+}
+
+
+console.log(studentDetail);
