b88454b4b2ffeef09620b8d731b4cfde50f5622a
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-10 11:12:55 +0530

    object5

--- Javascript/object5.js
+++ Javascript/object5.js
@@ -1,8 +1,9 @@
-let myname = 'Riya';
-String.prototype.upper = function (){
-    console.log(`It is changed to uppercase: ${this.toUpperCase()}`)
-}
+// let myname = 'Riya';
+// String.prototype.upper = function (){
+//     console.log(`It is changed to uppercase: ${this.toUpperCase()}`)
+// }
 
-let tiwari = 'tiwari'
-myname.upper();
-tiwari.upper();
+// let tiwari = 'tiwari'
+// myname.upper();
+// tiwari.upper();
+
