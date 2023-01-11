462d70c98333b06973df778e7fb0809f48b7be63
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-11 17:13:41 +0530

    6,7 updated

--- Javascript Assignment/Q6,7.js
+++ Javascript Assignment/Q6,7.js
@@ -1,7 +1,7 @@
 // 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
 
 let stringVar = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
-// console.log(stringVar.split(","))
+console.log(stringVar.split(","))
 
 // Determining the position of the first and last occurrence of a word "pw skills" using lastIndexOf in an array
 // containing multiple values.
---
+++ Javascript Assignment/Q8,9.js
@@ -0,0 +1,16 @@
+// Using trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+let stringVar = "  Riya    ";
+console.log(stringVar.trim())
+
+// Boolean value is either true or false.
+
+// Writing three JavaScript statement examples which provide truthy value.
+
+const multiplyCheck =  function (num1, num2){
+    if (num1*num2 == 70){
+        return true
+    }
+}
+
+console.log(multiplyCheck(7, 10));
