1b837f0e35efc9a35c26899e14c3d473edd8287e
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 02:22:08 +0530

    Q10,11-done

--- Javascript Assignment/Q10,11.js
+++ Javascript Assignment/Q10,11.js
@@ -31,5 +31,48 @@ const i = 4 == '4';
 const j = 4 === '4';
 // false
 
+// console.log(j)
 
-console.log(j)
+// Use the Date object to do the following activities
+
+// What is the year today?
+
+const current = new Date();
+const year = current.getFullYear();
+// console.log(year)    
+
+// What is the month today as a number?
+
+const month = current.getMonth();
+// console.log(month) 
+
+// What is the date today?
+
+const date = current.getDate();
+// console.log(date)
+
+// What is the day today as a number?
+
+const dayToday = current.getDay();
+// console.log(dayToday)
+
+// What is the hours now?
+
+const hoursNow = current.getHours();
+// console.log(hoursNow)
+
+// What is the minutes now?
+
+const minutesNow = current.getMinutes();
+// console.log(minutesNow)
+
+// Find out the numbers of seconds elapsed from January 1, 1970 till now.
+
+Date.prototype.secondsElapsed = function (){
+    const today = this.getTime();
+    const secondsFrom = Math.floor(today / 1000)
+    console.log(secondsFrom);
+}
+
+const x = new Date ();
+x.secondsElapsed() 
