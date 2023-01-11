3c83a2b285a3fe3a1a3d50596e6384df5ec89363
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-11 14:23:15 +0530

    Q1

---
+++ Javascript Assignment/Q1.js
@@ -0,0 +1,15 @@
+// Q1.Assigning string, boolean, undefined and null data types, Displaying all the value with their data type.
+
+// console.log(typeof 'Riya');
+
+// console.log(typeof 77);
+
+// console.log(typeof true);
+
+// console.log(typeof undeclaredVariable);
+
+// console.log(typeof null);
+
+// console.log(typeof function () {});
+
+// console.log(typeof Symbol('symbol in JS'));
