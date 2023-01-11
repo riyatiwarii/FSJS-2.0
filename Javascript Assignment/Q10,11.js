5873cd0718db2bb70877e9fca9e869a37599f213
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 01:16:09 +0530

    10

---
+++ Javascript Assignment/Q10,11.js
@@ -0,0 +1,35 @@
+// Result of the following comparison expression first without using console.log(). 
+// After you decide the result confirm it using console.log()
+
+const a = 4 > 3;
+// true
+
+const b = 4 >= 3;
+// true
+
+const c = 4 < 3;
+// false
+
+const d = 4 <= 3;
+// false
+
+const e = 4 == 4;
+// true
+
+const f = 4 != 4;
+// false
+
+const g = 4 !== 4;
+// false
+
+const h = 4 != '4';
+// false
+
+const i = 4 == '4';
+// true
+
+const j = 4 === '4';
+// false
+
+
+console.log(j)
