d152d1e15721f7117ad93ef9f19e8d3ee35f9949
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-11 14:45:08 +0530

    Q2-done

--- Javascript Assignment/Q2.js
+++ Javascript Assignment/Q2.js
@@ -1,2 +1,15 @@
 // Using interploation method, displaying variables storing first name, last name, marital status, country.
 
+// In this question, to perform the string interpolation, this template literal ${any_varibale} expression wrapped in backticks will work.
+
+const personDetail = {
+    firstName : 'Riya',
+    lastName : 'Tiwari',
+    maritalStatus : 'Not married',
+    country : 'India',
+    showStatus : function () {
+        console.log(`Your status: ${this.firstName}, ${this.lastName}, martital status: ${this.maritalStatus}, from  Country ${this.country}`)
+    }
+}
+
+personDetail.showStatus();
