56ea1f2252f9396a52e3c668a8ecc1b4f3799b72
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-11 17:05:16 +0530

    Q7 added

--- Javascript Assignment/Q6,7.js
+++ Javascript Assignment/Q6,7.js
@@ -1,5 +1,16 @@
 // 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
 
 let stringVar = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
-console.log(stringVar.split(","))
+// console.log(stringVar.split(","))
+
+// Determining the position of the first and last occurrence of a word "pw skills" using lastIndexOf in an array
+// containing multiple values.
+
+const tech = ["html", "css", "javascript", "pw skills"]
+// finding index of last occurence
+console.log(tech.lastIndexOf("pw skills"))
+
+// finding index of first occurence
+const tech1 = ["pw skills","html", "css", "javascript"]
+console.log(tech1.lastIndexOf("pw skills"))
 
