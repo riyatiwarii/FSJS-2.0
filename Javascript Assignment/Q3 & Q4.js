933efc440a111e1a5193c9f4e00dc7b31420c571
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-11 14:53:00 +0530

    Q4 added

--- Javascript Assignment/Q3 & Q4.js
+++
@@ -1,5 +0,0 @@
-// Declaring a varibale and assigning string value to it.
-// Changing all the string characters to capital letters using toUpperCase() method.
-
-const stringVar = 'Riya';
-console.log(`Changed to upperCase: ${stringVar.toUpperCase()}`)
