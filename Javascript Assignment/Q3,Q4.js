b87a089b7ed8f5ed6316913cc51c7e3247ed5da4
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-11 15:13:51 +0530

    Q5 added

--- Javascript Assignment/Q3,Q4.js
+++ Javascript Assignment/Q3,Q4.js
@@ -7,3 +7,9 @@ console.log(`Changed to upperCase: ${stringVar.toUpperCase()}`)
 // Checking if the string contains a word Script using includes() method.
 
 console.log(`Checking: ${stringVar.includes("y")}`);
+
+// Splitting the string into an array using split() method.
+
+splitted = stringVar.split("");
+console.log(`String splitted into an Array: ${JSON.stringify(splitted)}`);
+console.log(splitted)
