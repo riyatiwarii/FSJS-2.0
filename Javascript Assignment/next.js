7518538c8c0e4b923716e465777fde5c393c3731
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 15:34:04 +0530

    16

--- Javascript Assignment/next.js
+++ Javascript Assignment/next.js
@@ -4,3 +4,14 @@
 //     - March, April or May, the season is Spring.
 //     - June, July or August, the season is Summer.
 
+userInputMonth= prompt("Enter the month to check the season.")
+
+if (userInputMonth === 'September' || userInputMonth === 'October' || userInputMonth === "Novermber"){
+    console.log('The season is Autumn');
+} else if (userInputMonth === 'December' || userInputMonth ==='January' || userInputMonth === "February"){
+    console.log('The season is Winter');
+} else if (userInputMonth === 'March' || userInputMonth === 'April' || userInputMonth === "May"){
+    console.log('The season is Spring');
+} else if (userInputMonth === 'June' || userInputMonth === 'July' || userInputMonth === "August"){
+    console.log('The season is Summer');
+}
