d90fb1b7bda553e4cbafa153414ffb713096440f
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 14:35:03 +0530

    Q.13

--- Javascript Assignment/Q13,14,15.js
+++ Javascript Assignment/Q13,14,15.js
@@ -1,3 +1,11 @@
+// Get user input using prompt(“Enter your age:”). If user is 18 or older,
+// give feedback:'You are old enough to drive' but if not 18 give another feedback
+// stating to wait for the number of years he needs to turn 18.
+
 let userInput = prompt("Enter your age to check your driving eligibility.")
 
-console.log(userInput)
+if (userInput < 18){
+    console.log("Hey! You're not old enough to drive.")
+}else{
+    console.log("You're old enough to drive. Celebrate it!")
+}
