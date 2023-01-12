5c9368ee2b0d274d94f76e2dd7632909f0c960df
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 17:03:08 +0530

    16,17,18

--- Javascript Assignment/Q16,17,18.js
+++ Javascript Assignment/Q16,17,18.js
@@ -4,7 +4,7 @@
 //     - March, April or May, the season is Spring.
 //     - June, July or August, the season is Summer.
 
-userInputMonth= prompt("Enter the month to check the season.")
+// userInputMonth= prompt("Enter the month to check the season.")
 
 if (userInputMonth === 'September' || userInputMonth === 'October' || userInputMonth === "Novermber"){
     console.log('The season is Autumn');
@@ -15,3 +15,20 @@ if (userInputMonth === 'September' || userInputMonth === 'October' || userInputM
 } else if (userInputMonth === 'June' || userInputMonth === 'July' || userInputMonth === "August"){
     console.log('The season is Summer');
 }
+
+// A program which tells the number of days in a month.
+
+// const inputMonth = prompt("Enter the month to find the number of days in it");
+
+function getDaysInMonth (month, year) { 
+    return new Date(year, month, 0).getDate();
+  }
+
+// May in a non-leap year.
+console.log(getDaysInMonth(5, 2023));  
+
+// February in a non-leap year.
+console.log(getDaysInMonth(2, 2023));
+
+// February in a leap year.
+console.log(getDaysInMonth(2, 2000));
