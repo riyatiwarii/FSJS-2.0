22aba1403b5fc616a006e6d18c6e7d4ddff9ad8a
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 14:39:43 +0530

    14-updated

--- Javascript Assignment/Q13,14,15.js
+++ Javascript Assignment/Q13,14,15.js
@@ -2,10 +2,22 @@
 // give feedback:'You are old enough to drive' but if not 18 give another feedback
 // stating to wait for the number of years he needs to turn 18.
 
-let userInput = prompt("Enter your age to check your driving eligibility.")
+// let userInput = prompt("Enter your age to check your driving eligibility.")
 
-if (userInput < 18){
-    console.log("Hey! You're not old enough to drive.")
-}else{
-    console.log("You're old enough to drive. Celebrate it!")
-}
+// if (userInput < 18){
+//     console.log("Hey! You're not old enough to drive.")
+// }else{
+//     console.log("You're old enough to drive. Celebrate it!")
+// }
+
+// Even numbers are divisible by 2 and the remainder is zero. How do you check,
+// if a number is even or not using JavaScript? Create a program which checks that
+// the given number is even or odd.
+
+// let userNum = prompt("Enter your number to check if it is even or odd number.")
+
+// if (userNum%2 === 0){
+//     console.log('It is an even number.')
+// }else{
+//     console.log('It is an odd number.')
+// }
