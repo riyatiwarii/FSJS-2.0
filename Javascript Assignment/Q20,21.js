d42633eb29e70374e2025b06a3cfce9b69b833a1
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 23:54:17 +0530

    20,21

--- Javascript Assignment/Q20,21.js
+++ Javascript Assignment/Q20,21.js
@@ -19,6 +19,16 @@ const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
 // console.log(shoppingCart.splice(3, 1, "Green Tea"));
 // console.log(shoppingCart)
 
+// In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'.
+// If it does not exist add to the countries list.
 
-
+const countries = ["Australia", "Ethiopia", "New Zealand"];
+let country = "Ethiopia"
+const check = countries.includes(country);
+if (check){
+    console.log(country.toUpperCase());
+} else{
+    const include = countries.push(country)
+    console.log(countries)
+}
 
