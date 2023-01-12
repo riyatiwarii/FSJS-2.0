c637ee270e71c7e4d3d9243cbfa3cf2a6d4b043b
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-13 01:50:24 +0530

    23

---
+++ Javascript Assignment/Q23,24.js
@@ -0,0 +1,22 @@
+// A program to check that the number given by the user is a prime number or not.
+
+// const userInputNumber = prompt("Please enter number to check it it is prime number or not.")
+
+// if (userInputNumber % 2 == 0){
+//     if (userInputNumber == 2){
+//         console.log("It is a prime number.")
+//     }
+//     else{
+//         console.log("It is not a prime number.")
+//     }
+// } else{
+//     if (userInputNumber < 2){
+//         console.log("It is not a prime number.")
+//     }
+//     else{
+//         console.log("It is a prime number")
+//     }
+// }
+
+//  A program to create two array "even" and "odd" having even and odd number
+//  between the 0 to 100 (0 and 100 included).
