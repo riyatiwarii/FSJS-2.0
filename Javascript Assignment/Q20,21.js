47eddff453978b2a248ad6fc4f9616098b2323d5
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-12 20:12:27 +0530

    20-done

---
+++ Javascript Assignment/Q20,21.js
@@ -0,0 +1,20 @@
+// In the following shopping cart add, remove, edit items
+//     => const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//     - add 'Meat' in the beginning of your shopping cart if it has not been already added
+//     - add Sugar at the end of your shopping cart if it has not been already added
+//     - remove 'Honey'
+//     - modify Tea to 'Green Tea'
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+
+console.log(shoppingCart.unshift("Meat"));
+// console.log(shoppingCart);
+// [ 'Meat', 'Milk', 'Coffee', 'Tea', 'Honey' ]
+console.log(shoppingCart.push("Sugar"));
+// console.log(shoppingCart);
+// [ 'Meat', 'Milk', 'Coffee', 'Tea', 'Honey', 'Sugar' ]
+console.log(shoppingCart.splice(4, 1));
+// console.log(shoppingCart);
+// [ 'Meat', 'Milk', 'Coffee', 'Tea', 'Sugar']
+console.log(shoppingCart.splice(3, 1, "Green Tea"));
+console.log(shoppingCart)
