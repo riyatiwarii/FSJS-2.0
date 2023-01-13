1799683b09451bcd4569745a3ea3a98cee600028
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-13 12:35:09 +0530

    24-add

--- Javascript Assignment/Q23,24.js
+++ Javascript Assignment/Q23,24.js
@@ -20,3 +20,20 @@
 
 //  A program to create two array "even" and "odd" having even and odd number
 //  between the 0 to 100 (0 and 100 included).
+
+var evenArray = [];
+for (var i = 1; i <= 100; i++) {
+    if(i % 2 == 0){
+        evenArray.push(i)
+    }
+}
+console.log(evenArray)
+
+var oddArray = [];
+for (var i = 1; i <= 100; i++) {
+    if(i % 2 != 0){
+        oddArray.push(i)
+    }
+}
+console.log(oddArray)
+
