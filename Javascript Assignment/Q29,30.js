4436efb6caffd47e9d9d601cbceb8920c3b87977
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-14 01:25:50 +0530

    30

--- Javascript Assignment/Q29,30.js
+++ Javascript Assignment/Q29,30.js
@@ -12,3 +12,33 @@
 // Create a simple calculator program in JavaScript which can perform the
 // addition, substraction, multiplication and division on given numbers.
 
+const operator = prompt("Enter the operator to perform the calculation.")
+
+const num1 = parseFloat(prompt("Enter the first number"))
+const num2 = parseFloat(prompt("Enter the second number"))
+
+switch(operator) {
+    case '+':
+        result = num1 + num2;
+        console.log(`The result is: ${result}`);
+        break;
+
+    case '-':
+        result = num1 - num2;
+        console.log(`The result is: ${result}`);
+        break;
+
+    case '*':
+        result = num1 * num2;
+        console.log(`The result is: ${result}`);
+        break;
+
+    case '/':
+        result = num1 / num2;
+        console.log(`The result is: ${result}`);
+        break;
+
+    default:
+        console.log('Enter the correct operator, it was invalid.');
+        break;
+}
