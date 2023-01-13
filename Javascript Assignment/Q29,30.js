151a854ec7db7d25b872eead33065b702844115e
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-14 01:11:52 +0530

    29-edited

---
+++ Javascript Assignment/Q29,30.js
@@ -0,0 +1,14 @@
+// node Q29,30.js
+
+// Write a javascript program which takes the input of filename as string and 
+// prints the extension of the file in the console.
+
+// let filename = prompt("Enter the file name")
+
+// let printExtension = filename.split(".").pop();
+
+// console.log(printExtension);
+
+// Create a simple calculator program in JavaScript which can perform the
+// addition, substraction, multiplication and division on given numbers.
+
