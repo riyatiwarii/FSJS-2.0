d5557e159c2036f8432c09d58689cc465f657ad4
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-13 15:15:52 +0530

    26

--- Javascript Assignment/Q25,26.js
+++ Javascript Assignment/Q25,26.js
@@ -9,23 +9,38 @@
 //     - Overweight: BMI is 25 to 29.9
 //     - Obese: BMI is 30 or more
 
-let userHeight = prompt("Enter your height(in cm)")
-let userWeight = prompt("Enter your weight(in kg)")
+// let userHeight = prompt("Enter your height(in cm)")
+// let userWeight = prompt("Enter your weight(in kg)")
 
-function calculateBMI (height, weight){
-    let bmi = (weight/(height*height/10000))
-    if (bmi < 18.5){
-        console.log("Your are underweight according to BMI Index.");
-    }else if(bmi >= 18.5 && bmi <= 24.9){
-        console.log("Your weight is normal according to BMI Index.");
-    }else if(bmi >= 25 && bmi <= 29.9){
-        console.log("Your are overweight according to BMI Index.");
-    }else if(bmi > 30){
-        console.log("Your are obese according to BMI Index.");
-    }
+// function calculateBMI (height, weight){
+//     let bmi = (weight/(height*height/10000))
+//     if (bmi < 18.5){
+//         console.log("Your are underweight according to BMI Index.");
+//     }else if(bmi >= 18.5 && bmi <= 24.9){
+//         console.log("Your weight is normal according to BMI Index.");
+//     }else if(bmi >= 25 && bmi <= 29.9){
+//         console.log("Your are overweight according to BMI Index.");
+//     }else if(bmi > 30){
+//         console.log("Your are obese according to BMI Index.");
+//     }
+// }
+
+// calculateBMI(userHeight, userWeight);
+
+// Write a program to print the table of any number given by the user.
+// The format of the output should be smiliar to the below example-
+// 	If the number given by the user is 2 then the output should look like this-
+// 	2 * 1 = 2
+// 	2 * 2 = 4
+// 	2 * 3 = 6 and so on till 2 * 10 = 20.
+
+let num = prompt("Enter the number for its multiplication table.")
+
+for (let i = 1; i <= 10; i++){
+    
+    console.log(`${num} * ${i} = ${num*i}`)
+
 }
-
-calculateBMI(userHeight, userWeight);
 
 
 
