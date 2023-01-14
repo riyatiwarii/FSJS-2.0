504735dcfad97ab2d1dd73756cc31461064c38bd
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-14 14:09:16 +0530

    second-way-added

--- Javascript Assignment/Q25,26,27.js
+++ Javascript Assignment/Q25,26,27.js
@@ -45,6 +45,8 @@
 
 // Write a program to print all the prime number between 0 to 100 (0 and 100 included).
 
+// First way
+
 let checkPrimeNumber = true;
 for (let i = 2; i <= 100; i++) {
     for (let factor = 2; factor < i-1; factor++) {
@@ -58,6 +60,26 @@ for (let i = 2; i <= 100; i++) {
     }
     checkPrimeNumber = true
 }
+
+// Second way
+
+// function checkPrimeNumber(n) {
+//     if (n <= 1) {
+//         return false;
+//     }
+//     for (let i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//     if (checkPrimeNumber(i)) {
+//         console.log(i);
+//     }
+// }
 
 
 
