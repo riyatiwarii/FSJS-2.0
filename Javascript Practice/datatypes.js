a929614cfae437211dd07b00bf2665f4c5bb8e64
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-16 13:57:39 +0530

    commit

--- Javascript Practice/datatypes.js
+++ Javascript Practice/datatypes.js
@@ -4,61 +4,36 @@
 
 // String, Symbol, Number, Null, Undefined, BigInt, Boolean
 
-// 😨
-// 🤓
+// https://javascriptrefined.io/the-wrapper-object-400311b29151
 
 // String
-// let person = "Riya";
-// console.log(person.toUpperCase());
 
-// How does .toUpperCase() work, though? Initially you might think that strings are really objects in disguise
-// and try assigning properties to them. But that doesn't work.
+let x = "Riya";
+let y = String("JavaScript");
+let z = new String("HtmlCSS");
 
-// If we are treating strings as an object then let's try to assign it a property "favouriteThing."
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
 
-// let person = "Riya"
-// person.favouriteThing = "Mousse cake"
+// Accessing existing properties/methods
 
-// console.log(person.favouriteThing);
+// console.log(x.length);
+// console.log(x.toUpperCase()); 
+// console.log(z.length);
+// console.log(z.toUpperCase());
 
-// It doesn't work but still it also doesn't trigger any error and returns "undefined"
-// My dear friend, we know that Javascript acts like the smartest guy and in order to allow you
-// to do this thing, it does something tricky. Let's find out!
-
-let person = new String ("Riya")
-console.log(person);
-console.log(typeof person);
-
-person.favouriteThing = "Mousse cake"
-
-console.log(person.favouriteThing);
-
-// Wrapper Objects
-
-// All primitive types except null and undefined have their corresponding wrapper objects.
+// Setting new properties/methods
 
 
+// console.log(Object.getOwnPropertyDescriptor(z,'length'));
+// console.log(Object.getOwnPropertyDescriptor(z, 'check'));
 
 // Symbol
 
 
 // Number
 
-// console.log(777 / "Riya")
-
-// NaN (Not a Number) simply represents a value which could not be determined by computer arithmetic.
-
-// console.log(0 / 0)
-// console.log(0*Infinity)
-// typeof(NaN)
-
-// It's datatype in number. However, it is not a real number and
-// it represents any number outside of the ECMAScript.
-
-// NaN < 1;    
-// NaN > 1;   
-// NaN == NaN; 
-// isNaN(NaN)
 
 // Null
 
