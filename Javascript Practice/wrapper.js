b2dd80d1621859c5b0670f1449c32b90ce74bd9c
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-19 20:06:29 +0530

    commit

--- Javascript Practice/wrapper.js
+++ Javascript Practice/wrapper.js
@@ -3,7 +3,7 @@
 
 // From the very beginning, the 'var' keyword was used in JavaScript programming whereas the keyword 'let' was just added in 2015.
 // The keyword 'Var' has a function scope. Anywhere in the function, the variable specified using var is accessible but in ‘let’ the scope of a variable declared with the 'let' keyword is limited to the block in which it is declared. Let's start with a Block Scope.
-// In ECMAScript 2015, let and const are hoisted but not initialized. Referencing the variable in the block before the variable declaration results in a ReferenceError because the variable is in a "temporal dead zone" from the start of the block until the declaration is processed.
+// In ECMAScript 2015, let and const are hoisted but not initialized. Referencing the variable in the block before the variable declaration results in a ReferenceError because the variable is in a "temporal dead zone" from the start of the block until the declaration is processed...
 
 
 
