61cf3e0719fbc0767eac292280c07ff52419136d
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-19 20:06:16 +0530

    edit-1

--- Javascript Practice/wrapper.js
+++ Javascript Practice/wrapper.js
@@ -1,28 +1,11 @@
-// let a = undefined;
+// 5. Difference between var and let keyword in javascript.
+// Some differences are 
 
-// if (typeof a === 'undefined') {
-//     console.log("a is declared");
-//   } else {
-//     console.log("a is declared and initialized");
-//   }
-
-// if (a === undefined) {
-//     console.log("a is declared");
-//   } else {
-//     console.log("a is declared and initialized");
-//   }
+// From the very beginning, the 'var' keyword was used in JavaScript programming whereas the keyword 'let' was just added in 2015.
+// The keyword 'Var' has a function scope. Anywhere in the function, the variable specified using var is accessible but in ‘let’ the scope of a variable declared with the 'let' keyword is limited to the block in which it is declared. Let's start with a Block Scope.
+// In ECMAScript 2015, let and const are hoisted but not initialized. Referencing the variable in the block before the variable declaration results in a ReferenceError because the variable is in a "temporal dead zone" from the start of the block until the declaration is processed.
 
 
-var myname = "Riya";
 
-// console.log(mySurname);
-
-function myScope () {
-    var mySurname = "Tiwari";
-    return mySurname
-}
-// myScope();
-console.log(myScope());
-console.log(mySurname);
 
 
