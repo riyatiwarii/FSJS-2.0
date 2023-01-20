ec2ebe80e78a673d0a779d9fe69a33d1ed091468
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-20 17:03:27 +0530

    commit

--- Javascript Practice/wrapper.js
+++ Javascript Practice/wrapper.js
@@ -1,3 +1,21 @@
+Stack Overflow
+// Stack overflow happens when we call functions nested inside each, other over and over again.
+// If we just keep adding functions to the stack without popping them off,
+// we will have a stack overflow.
+// We can create a stack overflow very easily with recursion
+// function sayHello(){
+//   console.log("Riya says Hello");
+//   sayHello();
+// }
+// sayHello();
+
+function doLoop(){
+  for (let i = 0; i < 5; i++){
+    console.log("Loop is executing");
+  }
+  console.log("Loop is stopped.");
+}
+
 
 
 
