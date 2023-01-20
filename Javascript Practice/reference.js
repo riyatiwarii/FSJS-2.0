964a37dfbf32d0182928a7484804c1b71e2e0b23
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-20 18:52:22 +0530

    doLoop()

--- Javascript Practice/reference.js
+++
@@ -1,8 +0,0 @@
-let arr1 = [];
-let arr2 = [];
-
-let arr3 = arr1;
-console.log(arr3 === arr1);;
-
-
-console.log(`string literal wrapper is a statement and this --> is expression ${c = 2 + 3 }`)
---
+++ Javascript Practice/scope.js
@@ -0,0 +1,8 @@
+// function doLoop(){
+//     for (var i = 0; i < 5; i++){
+//       console.log(`Loop is executing ${i} time.`);
+//     }
+//     console.log(`Loop is stopping finally at ${i} time.`);
+//   }  
+// doLoop();
+
