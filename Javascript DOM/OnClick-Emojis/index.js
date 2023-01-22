c497194e7c2c3335d09bbb7f51a43ab47a3c5b27
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-22 15:24:29 +0530

    styling-added

--- Javascript DOM/OnClick-Emojis/index.js
+++ Javascript DOM/OnClick-Emojis/index.js
@@ -1,15 +1,22 @@
 const btn = document.getElementsByTagName("button")[0];
 const onBody = document.getElementsByTagName("body")[0];
-
+const heading = document.createElement("h1")
+heading.innerText = "Click anywhere and create the emojies."
+onBody.appendChild(heading)
+console.log(heading);
 onBody.addEventListener('click', function(event){
     const emoji = document.createElement("div");
     emoji.classList.add("emoji-pop")
-const emojisList = ['😂', '😎', '😛', '😁', '😍', '🫡', '😥', '😅','😉', '😑', '🥵', '🥲', '😋', '🤨', '🙄'];
+const emojisList = ['😂', '😛', '😁', '😍', '🫡', '😥', '😅','😉', '😑', '🥵', '🥲', '😋', '🤨', '🙄'];
     a = event.clientX;
     b = event.clientY;
     let x = Math.floor(Math.random() * emojisList.length)    
     emoji.innerHTML = emojisList[x]
     emoji.style.left = a - 35 + "px";
     emoji.style.top = b - 35 + "px";
-    onBody.appendChild(emoji)
+    onBody.appendChild(emoji);
+    setTimeout(() => {
+        emoji.style.transform = "scale(0)"
+    }, 250)
+    onBody.removeChild(heading);
 })
