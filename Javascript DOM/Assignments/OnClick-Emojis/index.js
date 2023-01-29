bad629ce4362bb97c64d3fb817751558fec13c6a
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-30 00:16:39 +0530

    removeChild-syntax-update

--- Javascript DOM/Assignments/OnClick-Emojis/index.js
+++ Javascript DOM/Assignments/OnClick-Emojis/index.js
@@ -1,21 +1,26 @@
 const onBody = document.getElementsByTagName("body")[0];
-
 const heading = document.createElement("h1")
 heading.innerText = "Click anywhere and create the emojies."
 heading.style.textAlign = "center";
 onBody.appendChild(heading)
-// console.log(heading);
 onBody.addEventListener('click', function(event){
     const emoji = document.createElement("div");
     emoji.classList.add("emoji-pop")
 const emojisList = ['😂', '😛', '😁', '😍', '🫡', '😥', '😅','😉', '😑', '🥵', '🥲', '😋', '🤨', '🙄'];
     let x = Math.floor(Math.random() * emojisList.length)    
-    emoji.innerHTML = emojisList[x]
+    emoji.innerText = emojisList[x]
+    onBody.appendChild(emoji)
     emoji.style.left = event.clientX - 35 + "px";
     emoji.style.top = event.clientY - 35 + "px";
-    onBody.appendChild(emoji);
+    if (heading.parentNode) {
+        heading.parentNode.removeChild(heading);
+      }
     setTimeout(() => {
         emoji.style.transform = "scale(0)"
     }, 2000)
-    onBody.removeChild(heading);
 })
+
+
+
+
+
