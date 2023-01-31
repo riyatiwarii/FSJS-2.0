c7a0f3290d9e17fe43b9aa07393f03aa0fdb32fc
Riya Tiwari <riyatiwari727@gmail.com>
2023-01-31 15:58:49 +0530

    task-1-done

---
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/secondAssignmentImage/task1.js
@@ -0,0 +1,4 @@
+const accordianHeading = Array.from(document.querySelectorAll(".accordian h3"));
+accordianHeading.map((ele)=> ele.style.backgroundColor = "#dadaf8");
+const accordianPara = Array.from(document.querySelectorAll(".accordian p"));
+accordianPara.map((ele)=> ele.style.backgroundColor = "#eeeeff");
