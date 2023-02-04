c0f4fc6f4d2b16fc1e498e2e5459c059e8151f48
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-04 12:38:16 +0530

    counter-restart-added

--- Javascript Practice/Closure/Part 2/index.js
+++ Javascript Practice/Closure/Part 2/index.js
@@ -3,19 +3,19 @@ const zeroBtn = document.getElementsByTagName("button")[1]
 
 // let counter = 0
 
-function incrementCounter () {
-    let counter = 0
-    function increment () {
-        counter++
-        console.log(counter);
-        counterBtn.innerText = counter
-    }
-    return increment
-}
+// function incrementCounter () {
+//     let counter = 0
+//     function increment () {
+//         counter++
+//         console.log(counter);
+//         counterBtn.innerText = counter
+//     }
+//     return increment
+// }
 
-const retainCount = incrementCounter()
-counter = 10
-counterBtn.addEventListener("click", retainCount)
+// const retainCount = incrementCounter()
+// counter = 10
+// counterBtn.addEventListener("click", retainCount)
 
 // zeroBtn.addEventListener("click", backToZero)
 
@@ -24,30 +24,25 @@ counterBtn.addEventListener("click", retainCount)
 //     counterBtn.innerText = counter
 // }
 
-// function buttonCounter () {
-//     let counter = 0
-//     function incrementCounter () {
-//         counter++
-//         counterBtn.innerText = counter
-//         console.log(counter);
-//     }
-//     function restartCounter() {
-//         counter = 0
-//         counterBtn.innerText = counter
-//         console.log(counter);
-//     }
-//     return {
-//         incrementCounter: incrementCounter,
-//         restartCounter: restartCounter,
-//     };
-// }
+function buttonCounter () {
+    let counter = 0
+    function incrementCounter () {
+        counter++
+        counterBtn.innerText = counter
+    }
+    function restartCounter() {
+        counter = 0
+        counterBtn.innerText = counter
+    }
+    return {
+        incrementCounter: incrementCounter,
+        restartCounter: restartCounter,
+    };
+}
 
-// const a = buttonCounter()
-// a.incrementCounter();
-// a.incrementCounter();
-// a.incrementCounter();
-// console.log(a);
-// a.restartCounter();
+const retainCount = buttonCounter()
 
-// counterBtn.addEventListener("click", a.incrementCounter)
-// zeroBtn.addEventListener("click", a.restartCounter)
+counterBtn.addEventListener("click", retainCount.incrementCounter)
+zeroBtn.addEventListener("click", retainCount.restartCounter)
+
+counter = 10;
