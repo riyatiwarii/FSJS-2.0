c3ab04485d560377ae3332d0c7d52d23e61ecbee
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-04 10:28:46 +0530

    task-2 done

--- 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/thirdAssignmentImage/task1.js
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/thirdAssignmentImage/task1.js
@@ -1,16 +1,3 @@
-// const enterName = document.getElementsByClassName("enterName")[0];
-// enterName.placeholder = "FSJS 2.0"
-// const enterMail = document.getElementsByClassName("enterMail")[0];
-// enterMail.placeholder = "fsjs@ineuron.ai"
-// const enterMessage = document.getElementsByClassName("enterMessage")[0];
-// enterMessage.placeholder = "Hello World"
-// const userName = document.getElementsByClassName("userName")[0];
-// userName.placeholder = "FSJS 2.0"
-// const userEmail = document.getElementsByClassName("userEmail")[0];
-// userEmail.placeholder = "fsjs@ineuron.ai"
-// const userMessage = document.getElementsByClassName("userMessage")[0];
-// userMessage.placeholder = "Hello World"
-
 function doTask (target, text) {
     document.getElementsByClassName(target)[0].placeholder = text
 }
