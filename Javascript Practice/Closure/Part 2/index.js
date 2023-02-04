725662c3d448458c693b9d1bb7c14ae4944504c6
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-04 13:37:27 +0530

    commit

--- Javascript Practice/Closure/Part 2/index.js
+++ Javascript Practice/Closure/Part 2/index.js
@@ -1,5 +1,5 @@
-const counterBtn = document.getElementsByTagName("button")[0]
-const zeroBtn = document.getElementsByTagName("button")[1]
+// const counterBtn = document.getElementsByTagName("button")[0]
+// const zeroBtn = document.getElementsByTagName("button")[1]
 
 // let counter = 0
 
@@ -24,25 +24,77 @@ const zeroBtn = document.getElementsByTagName("button")[1]
 //     counterBtn.innerText = counter
 // }
 
-function buttonCounter () {
-    let counter = 0
-    function incrementCounter () {
-        counter++
-        counterBtn.innerText = counter
+// function buttonCounter () {
+//     let counter = 0
+//     function incrementCounter () {
+//         counter++
+//         counterBtn.innerText = counter
+//     }
+//     function restartCounter() {
+//         counter = 0
+//         counterBtn.innerText = counter
+//     }
+//     return {
+//         incrementCounter: incrementCounter,
+//         restartCounter: restartCounter,
+//     };
+// }
+
+// const retainCount = buttonCounter()
+
+// counterBtn.addEventListener("click", retainCount.incrementCounter)
+// zeroBtn.addEventListener("click", retainCount.restartCounter)
+
+// counter = 10;
+
+
+
+
+
+
+// let count = 0;
+// function incrementCounter(){
+//     count++
+//     counterBtn.innerText = count;
+// }
+
+// counterBtn.addEventListener("click", incrementCounter)
+
+const counterBtn = document.getElementsByTagName("button")[0]
+const restartCounter = document.getElementsByTagName("button")[1]
+const decrementCounter = document.getElementsByTagName("button")[2]
+
+function counterWrapper () {
+    let count = 0
+    function incrementCounter(){
+        count++
+        counterBtn.innerText = count;
+}
+    function restartCounter(){
+        count = 0
+        counterBtn.innerText = count;
     }
-    function restartCounter() {
-        counter = 0
-        counterBtn.innerText = counter
+
+    function decrementCounter(){
+        if (count > 0){
+            count--
+            counterBtn.innerText = count;
+        }
+        
     }
+
     return {
-        incrementCounter: incrementCounter,
-        restartCounter: restartCounter,
-    };
+        incrementCounter : incrementCounter,
+        restartCounter : restartCounter,
+        decrementCounter : decrementCounter
+    }
+
 }
 
-const retainCount = buttonCounter()
+let store = counterWrapper()
 
-counterBtn.addEventListener("click", retainCount.incrementCounter)
-zeroBtn.addEventListener("click", retainCount.restartCounter)
+counterBtn.addEventListener("click", store.incrementCounter)
+restartCounter.addEventListener("click", store.restartCounter)
+decrementCounter.addEventListener("click", store.decrementCounter)
 
-counter = 10;
+count = 15;
