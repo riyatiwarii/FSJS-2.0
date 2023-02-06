9c610839ae7f5a964d1d8a9aea32abfd2c36c3be
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-06 11:41:14 +0530

    part-8

---
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P8/DOM P8/index.js
@@ -0,0 +1,18 @@
+const sideBar = document.getElementsByTagName("aside")[0];
+
+const heading = document.createElement("h2");
+const para = document.createElement("p");
+heading.textContent = "This is my custom heading";
+para.textContent = "This is my custom paragraph.";
+sideBar.appendChild(heading);
+sideBar.appendChild(para);
+heading.className = "new-head";
+para.className = "new-p";
+
+const horizontalLine = document.createElement("hr");
+horizontalLine.className = "hr-line";
+sideBar.appendChild(horizontalLine);
+
+sideBar.style.border = "4px solid red";
+sideBar.style.overflowY = "scroll";
+sideBar.style.overflowX = "hidden";
