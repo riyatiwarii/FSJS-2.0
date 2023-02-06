5dc1443b2d70fa7674d0f18ebaa0a983e31a5c76
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-06 20:06:12 +0530

    additional-to-4th

--- 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/index.js
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/index.js
@@ -3,3 +3,7 @@ const colors = ["#ec9b3b","#ee5487","#f6901a","#82bb30","#4facff"]
 for(let i=0; i<clashCards.length; i++){
     clashCards[i].style.backgroundColor = colors[i];
 }
+const oneThird = Array.from(document.querySelectorAll(".one-third"))
+oneThird.forEach((ele) => {
+    ele.style.color = "white"
+})
