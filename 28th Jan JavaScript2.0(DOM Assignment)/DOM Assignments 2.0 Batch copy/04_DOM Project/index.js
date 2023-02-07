14f0021fae3d387ab72dea5b32ec0bceddcff0a7
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-07 12:07:00 +0530

    minor-edit

--- 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/index.js
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/index.js
@@ -1,4 +1,4 @@
-const clashCards = Array.from(document.getElementsByClassName("clash-card__unit-stats"))
+const clashCards = Array.from((document.getElementsByClassName("clash-card__unit-stats")))
 const colors = ["#ec9b3b","#ee5487","#f6901a","#82bb30","#4facff"]
 for(let i=0; i<clashCards.length; i++){
     clashCards[i].style.backgroundColor = colors[i];
