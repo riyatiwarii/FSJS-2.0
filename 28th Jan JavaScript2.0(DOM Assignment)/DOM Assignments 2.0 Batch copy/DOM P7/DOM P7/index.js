69ac6ce45787e5361a9f78fd4b68e77fcb9f3bfe
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-08 19:48:39 +0530

    7th-second-part

--- 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P7/DOM P7/index.js
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P7/DOM P7/index.js
@@ -1,25 +1,47 @@
-let aTag = Array.from(document.getElementsByTagName("a"));
+let aTags = Array.from(document.getElementsByTagName("a"));
 const mainLanguages = document.getElementsByClassName("main__languages")[0]
 const input = document.getElementsByTagName("input")[0];
-const submit = document.getElementsByTagName("button")[2]
-submit.removeAttribute("disabled");
+const submitBtn = document.getElementsByTagName("button")[2]
+submitBtn.removeAttribute("disabled");
 input.removeAttribute("disabled");
 
-aTag.forEach(ele => {
+aTags.forEach(ele => {
     if (ele.innerText.includes("2.0")){
         mainLanguages.removeChild(ele)
     }
 })
 
-submit.addEventListener("click", doSumbit)
+let inputStore;
 
-function doSumbit(){
-input.value = "Riya"
-const a = document.createElement("a")
-a.setAttribute("href","https://www.ineuron.ai")
-a.setAttribute("target","_blank")
-a.innerText = input.value
-console.log(a);
-mainLanguages.appendChild(a)
+if(localStorage.getItem("inputs")){
+    inputStore = JSON.parse(localStorage.getItem("inputs"))
+} else {
+    inputStore = []
 }
+
+submitBtn.addEventListener("click", () => {
+    inputStore.push(input.value)
+    localStorage.setItem("inputs", JSON.stringify(inputStore))
+    appendInput(input.value);
+    
+})
+
+const appendInput = (inputTaken) => {
+    const aTag = document.createElement("a")
+    aTag.href = 'https://www.ineuron.ai';    
+    aTag.target = '_blank';
+    aTag.innerText = inputTaken
+    mainLanguages.appendChild(aTag)
+    console.log(aTag);
+}
+
+const getInput = JSON.parse(localStorage.getItem("inputs"));
+getInput.forEach((note) => {
+appendInput(note);
+});
+
+
+
+
+
 
